914cabb48cfc5e2fdedbd25027477970e228fbf7
David Gwilliam <dhgwilliam@gmail.com>
2012-08-23 17:54:34 -0400

    add simple check for mysql to avoid egregious errors

--- lib/facter/innodb.rb
+++ lib/facter/innodb.rb
@@ -1,8 +1,10 @@
 Facter.add(:innodb_buffer_pool_size) do
   confine :kernel => "Linux"
   setcode do
-    dbuser = Facter::Util::Resolution.exec('puppet master --configprint dbuser')
-    dbpassword = Facter::Util::Resolution.exec('puppet master --configprint dbpassword')
-    Facter::Util::Resolution.exec("mysql -rs -u#{dbuser} -p#{dbpassword} -e \'SELECT @@innodb_buffer_pool_size;\' | tail -n1")
+    if Facter::Util::Resolution.exec('which mysql').scan("no mysql").empty?
+      dbuser = Facter::Util::Resolution.exec('puppet master --configprint dbuser')
+      dbpassword = Facter::Util::Resolution.exec('puppet master --configprint dbpassword')
+      Facter::Util::Resolution.exec("mysql -rs -u#{dbuser} -p#{dbpassword} -e \'SELECT @@innodb_buffer_pool_size;\' | tail -n1")
+    end
   end
 end
